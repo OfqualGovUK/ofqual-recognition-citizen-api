4e5a009bf52ec95651110036820de7df427695e1
alcharra <168999056+alcharra@users.noreply.github.com>
2025-03-25 16:14:05 +0000

    [58011] FIX: add xml documentation to the new dto model

--- Ofqual.Recognition.Citizen.API.Core/Models/TaskItemStatuses/UpdateTaskStatusDto.cs
+++ Ofqual.Recognition.Citizen.API.Core/Models/TaskItemStatuses/UpdateTaskStatusDto.cs
@@ -3,6 +3,9 @@ using Ofqual.Recognition.Citizen.API.Core.Validations;
 
 namespace Ofqual.Recognition.Citizen.API.Core.Models;
 
+/// <summary>
+/// Represents an update to a task's status.
+/// </summary>
 public class UpdateTaskStatusDto
 {
     [ValidEnumValue(typeof(TaskStatusEnum), ErrorMessage = "The status provided is not valid.")]
