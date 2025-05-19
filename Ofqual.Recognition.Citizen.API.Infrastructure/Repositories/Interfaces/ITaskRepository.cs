af4527969ee9b98e4275c188e224f8546db04f45
Luke Hudson <Luke.Hudson@ofqual.gov.uk>
2025-05-19 11:18:19 +0100

    [58688] ADD: Missing Interface definition

--- Ofqual.Recognition.Citizen.API.Infrastructure/Repositories/Interfaces/ITaskRepository.cs
+++ Ofqual.Recognition.Citizen.API.Infrastructure/Repositories/Interfaces/ITaskRepository.cs
@@ -9,6 +9,8 @@ public interface ITaskRepository
     public Task<IEnumerable<ITaskItem>> GetAllTask();
     public Task<TaskItem?> GetTaskByTaskNameUrl(string taskNameUrl);
     public Task<IEnumerable<TaskItemStatusSection>> GetTaskStatusesByApplicationId(Guid applicationId);
+
+    public Task<IEnumerable<TaskItemStatusSection>> GetPreEngagementTasksByStageTaskId(Guid StageTaskId);
     public Task<bool> CreateTaskStatuses(Guid applicationId, IEnumerable<ITaskItem> tasks);
     public Task<bool> UpdateTaskStatus(Guid applicationId, Guid taskId, TaskStatusEnum status);
 }
