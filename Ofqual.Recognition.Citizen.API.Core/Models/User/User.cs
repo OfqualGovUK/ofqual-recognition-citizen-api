76e61bc9e1b496e86901e6b38ddb8881ca152a20
Toby Ward <toby.ward@ofqual.gov.uk>
2025-06-18 17:16:54 +0100

    [58710] ADD: User models

---
+++ Ofqual.Recognition.Citizen.API.Core/Models/User/User.cs
@@ -0,0 +1,20 @@
+﻿using Ofqual.Recognition.Citizen.API.Core.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class User : IDataMetadata
+{
+    public Guid UserId { get; set; }
+    public Guid B2CId { get; set; }
+    public Guid EmailAddress { get; set; }
+    public string DisplayName { get; set; }
+    public required string CreatedByUpn { get; set; }
+    public string? ModifiedByUpn { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+
+}
+
---
+++ Ofqual.Recognition.Citizen.API.Core/Models/User/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class UserDto
+{
+    public Guid UserId { get; set; }
+    public Guid B2CId { get; set; }
+    public Guid EmailAddress { get; set; }
+    public string DisplayName { get; set; }
+
+}
