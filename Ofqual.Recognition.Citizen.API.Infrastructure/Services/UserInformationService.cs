4e86e4b0d2940d7cf24532bfc3aed2b540eec1f5
Toby Ward <toby.ward@ofqual.gov.uk>
2025-06-20 11:58:08 +0100

    [58710] ADD: Unit tests for UserInformationService

--- Ofqual.Recognition.Citizen.API.Infrastructure/Services/UserInformationService.cs
+++ Ofqual.Recognition.Citizen.API.Infrastructure/Services/UserInformationService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Identity.Web;
 using System.Security.Claims;
 
 public class UserInformationService : IUserInformationService
@@ -10,17 +11,14 @@ public class UserInformationService : IUserInformationService
     }
     public string GetCurrentUserDisplayName()
     {
-        ClaimsPrincipal? user = _httpContextAccessor.HttpContext?.User;
-        return user?.FindFirstValue("name") ?? "Unavailable"; // legacy users may not have display names;
+        return _httpContextAccessor.HttpContext?.User?.GetDisplayName() ?? "Unavailable"; // legacy users may not have display names;
     }
 
     public string GetCurrentUserObjectId()
     {
-
-        ClaimsPrincipal? user = _httpContextAccessor.HttpContext?.User;
-        string? oid = user?.FindFirstValue(ClaimTypes.NameIdentifier); // Name Identifier is the Object ID
+        string? oid = _httpContextAccessor.HttpContext?.User.GetObjectId(); // Name Identifier is the Object ID
         if (oid == null) {
-            throw new ArgumentNullException("Exception raised when trying to obtain Object ID, in UserInformationService::GetCurrentUserObjectId. Exception message: No ObjectId claim found in access token");
+            throw new InvalidOperationException("Exception raised when trying to obtain Object ID, in UserInformationService::GetCurrentUserObjectId. Exception message: No ObjectId claim found in access token");
         }
         return oid; 
     }
@@ -30,7 +28,7 @@ public class UserInformationService : IUserInformationService
         ClaimsPrincipal? user = _httpContextAccessor.HttpContext?.User;
         string? email = user?.FindFirstValue("emails");
         if (email == null) {
-            throw new ArgumentNullException("Exception raised when trying to obtain upn, in UserInformationService::GetCurrentUserUpn. Exception message: No Email claim found in access token");
+            throw new InvalidOperationException("Exception raised when trying to obtain upn, in UserInformationService::GetCurrentUserUpn. Exception message: No Email claim found in access token");
         }
         return email;
     }
--- Ofqual.Recognition.Citizen.API.Tests/Unit/Services/UserInformationServiceTests.cs
+++ Ofqual.Recognition.Citizen.API.Tests/Unit/Services/UserInformationServiceTests.cs
@@ -1,20 +1,241 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Identity.Web;
 using Moq;
+using Newtonsoft.Json.Linq;
 using Ofqual.Recognition.Citizen.API.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 public class UserInformationServiceTests
 {
-    private readonly Mock<HttpContextAccessor> _mockHttpContextAccessor;
+    private readonly Mock<IHttpContextAccessor> _mockHttpContextAccessor;
+    private readonly UserInformationService _userInformationService;
 
     public UserInformationServiceTests()
     {
-        _mockHttpContextAccessor = new Mock<HttpContextAccessor>();
+        _mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+        _userInformationService = new UserInformationService(_mockHttpContextAccessor.Object);
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public Task GetCurrentUserDisplayName__WhenClaimAvailable__ReturnDisplayName()
+    {
+        // Arrange
+
+        // Set up claimsprincipal for user
+        string username = "Display Name";
+
+        List<Claim> claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.Name, username)
+        };
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "TestAuthType");
+        ClaimsPrincipal user = new ClaimsPrincipal(identity);
+
+        // Set up HTTP context and plug in the user
+
+        HttpContext context = new DefaultHttpContext();
+        context.User = user;
+
+
+        _mockHttpContextAccessor
+            .SetupGet(accessor => accessor.HttpContext).Returns(context);
+
+        // Act
+
+        var value = _userInformationService.GetCurrentUserDisplayName();
+
+        // Assert
+
+        Assert.Equal(username, value);
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public Task GetCurrentUserDisplayName__WhenClaimUnavailable__ReturnUnavailable()
+    {
+        // Arrange
+
+        // Set up claimsprincipal for user
+
+        List<Claim> claims = new List<Claim>();
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "TestAuthType");
+        ClaimsPrincipal user = new ClaimsPrincipal(identity);
+
+        // Set up HTTP context and plug in the user
+
+        HttpContext context = new DefaultHttpContext();
+        context.User = user;
+
+
+        _mockHttpContextAccessor
+            .SetupGet(accessor => accessor.HttpContext).Returns(context);
+
+        // Act
+
+        var value = _userInformationService.GetCurrentUserDisplayName();
+
+        // Assert
+
+        Assert.Equal("Unavailable", value);
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public Task GetCurrentUserObjectId__WhenClaimAvailable__ReturnObjectId()
+    {
+        // Arrange
+
+        // Set up claimsprincipal for user
+        string objectid = Guid.NewGuid().ToString();
+
+        List<Claim> claims = new List<Claim>()
+        {
+            new Claim(ClaimConstants.Oid, objectid)
+        };
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "TestAuthType");
+        ClaimsPrincipal user = new ClaimsPrincipal(identity);
+
+        // Set up HTTP context and plug in the user
+
+        HttpContext context = new DefaultHttpContext();
+        context.User = user;
+
+
+        _mockHttpContextAccessor
+            .SetupGet(accessor => accessor.HttpContext).Returns(context);
+
+        // Act
+
+        var value = _userInformationService.GetCurrentUserObjectId();
+
+        // Assert
+
+        Assert.Equal(objectid, value);
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public Task GetCurrentUserObjectId__WhenClaimUnavailable__ThrowInvalidOperationException()
+    {
+        // Arrange
+
+        // Set up claimsprincipal for user
+
+        List<Claim> claims = new List<Claim>();
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "TestAuthType");
+        ClaimsPrincipal user = new ClaimsPrincipal(identity);
+
+        // Set up HTTP context and plug in the user
+
+        HttpContext context = new DefaultHttpContext();
+        context.User = user;
+
+
+        _mockHttpContextAccessor
+            .SetupGet(accessor => accessor.HttpContext).Returns(context);
+
+        // Act + Assert
+
+        try
+        {
+            var value = _userInformationService.GetCurrentUserObjectId();
+        } catch (InvalidOperationException ex) {
+            Assert.Equal(
+                "Exception raised when trying to obtain Object ID, in UserInformationService::GetCurrentUserObjectId. Exception message: No ObjectId claim found in access token",
+                ex.Message
+            );
+            return Task.CompletedTask;
+        }
+
+        Assert.Fail();
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public Task GetCurrentUserUpn__WhenClaimAvailable__ReturnUpn()
+    {
+        // Arrange
+
+        // Set up claimsprincipal for user
+        string email = "test@test.com";
+
+        List<Claim> claims = new List<Claim>()
+        {
+            new Claim("emails", email)
+        };
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "TestAuthType");
+        ClaimsPrincipal user = new ClaimsPrincipal(identity);
+
+        // Set up HTTP context and plug in the user
+
+        HttpContext context = new DefaultHttpContext();
+        context.User = user;
+
+
+        _mockHttpContextAccessor
+            .SetupGet(accessor => accessor.HttpContext).Returns(context);
+
+        // Act
+
+        var value = _userInformationService.GetCurrentUserUpn();
+
+        // Assert
+
+        Assert.Equal(email, value);
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public Task GetCurrentUserUpn__WhenClaimUnavailable__ThrowInvalidOperationException()
+    {
+        // Arrange
+
+        // Set up claimsprincipal for user
+
+        List<Claim> claims = new List<Claim>();
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "TestAuthType");
+        ClaimsPrincipal user = new ClaimsPrincipal(identity);
+
+        // Set up HTTP context and plug in the user
+
+        HttpContext context = new DefaultHttpContext();
+        context.User = user;
+
+
+        _mockHttpContextAccessor
+            .SetupGet(accessor => accessor.HttpContext).Returns(context);
+
+        // Act + Assert
+
+        try
+        {
+            var value = _userInformationService.GetCurrentUserUpn();
+        }
+        catch (InvalidOperationException ex)
+        {
+            Assert.Equal(
+                "Exception raised when trying to obtain upn, in UserInformationService::GetCurrentUserUpn. Exception message: No Email claim found in access token",
+                ex.Message
+            );
+            return Task.CompletedTask;
+        }
+
+        Assert.Fail();
+        return Task.CompletedTask;
     }
 }
 
